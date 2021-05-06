01d01a5567db2a93e8b8f71644a02f15c319a80f
Jeremy Li <junzhli@gmail.com>
2021-05-06 12:32:14 +0800

    Add dotenv to enable .env based environment setting

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,11 @@
 import { IDBConfigOptions } from "./types/db";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const DBConfig: IDBConfigOptions = {
-    connectionUri: process.env.DB_CONNECTION_URI || "postgres://user:pass@example.com:5432/dbname"
+    connectionUri: process.env.DB_CONNECTION_URI || "postgres://user:pass@example.com:5432/dbname",
+    verboseLogging: (process.env.DB_DISABLE_LOGGING) ? false : true
 };
 
 export {
