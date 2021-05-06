285b05b2db5ad4b6ca4ea08c08b3779b961c98ad
Jeremy Li <junzhli@gmail.com>
2021-05-07 00:31:18 +0800

    Set additional config for adapting to heroku

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ import { IDBConfigOptions } from "./types/db";
 dotenv.config();
 
 const DBConfig: IDBConfigOptions = {
-    connectionUri: process.env.DATABASE_URL || "postgres://user:pass@example.com:5432/dbname",
+    connectionUri: process.env.HEROKU_POSTGRESQL_CHARCOAL_URL || process.env.DATABASE_URL || "postgres://user:pass@example.com:5432/dbname",
     verboseLogging: (process.env.DB_DISABLE_LOGGING) ? false : true,
     ssl: (process.env.DB_SSL) ? true : false
 };
